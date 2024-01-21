a57023c23da7a117d3913845a6bdc1bb1c49e00c
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2024-01-21 13:49:09 -0300

    5 e 6 concluidos

--- src/exercises/EX5.java
+++ src/exercises/EX5.java
@@ -1,5 +1,28 @@
 package exercises;
 
 public class EX5 {
-    
+    public static void main(String[] args) {
+
+        String[] array1 = {"Morango", "Banana", "Maçã", "Uva", "Caqui"};
+        String[] array2 = {"Manga", "Caqui", "Morango", "Amora"};
+
+        encontrarEImprimirElementosComuns(array1, array2);
+
+        Integer[] numeros1 = {1, 2, 3, 4, 5};
+        Integer[] numeros2 = {4, 5, 6, 7, 8};
+
+        encontrarEImprimirElementosComuns(numeros1, numeros2);
+    }
+
+    static void encontrarEImprimirElementosComuns(Object[] array1, Object[] array2) {
+        System.out.print("Elementos comuns: ");
+
+        for (Object elemento1 : array1) {
+            for (Object elemento2 : array2) {
+                if (elemento1.equals(elemento2)) {
+                    System.out.print(elemento1 + ", ");
+                }
+            }
+        }
+    }
 }
--- src/exercises/EX6.java
+++ src/exercises/EX6.java
@@ -1,5 +1,81 @@
 package exercises;
 
-public class EX6 {
-    
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+class Feriado {
+    private Date data;
+    private String nome;
+
+    public Feriado(String dataString, String nome) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        this.data = dateFormat.parse(dataString);
+        this.nome = nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
+
+public class EX6 {
+    private static List<Feriado> feriados = new ArrayList<>();
+
+    static {
+        try {
+            feriados.add(new Feriado("01-01-2024", "Confraternização Mundial"));
+            feriados.add(new Feriado("12-02-2024", "Carnaval"));
+            feriados.add(new Feriado("13-02-2024", "Carnaval"));
+            feriados.add(new Feriado("29-03-2024", "Sexta-feira Santa"));
+            feriados.add(new Feriado("21-04-2024", "Tiradentes"));
+            feriados.add(new Feriado("01-05-2024", "Dia do Trabalho"));
+            feriados.add(new Feriado("30-05-2024", "Corpus Christi"));
+            feriados.add(new Feriado("07-09-2024", "Independência do Brasil"));
+            feriados.add(new Feriado("12-10-2024", "Nossa Senhora Aparecida"));
+            feriados.add(new Feriado("02-11-2024", "Finados"));
+            feriados.add(new Feriado("15-11-2024", "Proclamação da República"));
+            feriados.add(new Feriado("20-11-2024", "Dia Nacional de Zumbi e da Consciência Negra"));
+            feriados.add(new Feriado("25-12-2024", "Natal"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void imprimirFeriados() {
+        for (Feriado feriado : feriados) {
+            System.out.println(feriado.getData() + " - " + feriado.getNome());
+        }
+    }
+
+    public static String verificaFeriado(String dataString) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date data = dateFormat.parse(dataString);
+
+        for (Feriado feriado : feriados) {
+            if (feriado.getData().equals(data)) {
+                return "Dia " + dataString + " é " + feriado.getNome() + "!";
+            }
+        }
+
+        return "Dia " + dataString + " não é feriado.";
+    }
+
+    public static void main(String[] args) {
+        imprimirFeriados();
+
+        try {
+            String mensagem = verificaFeriado("02-11-2024");
+            System.out.println(mensagem);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
